28e5a26e9ef5ea38ebb64eb227bad20848ccaf80
Doff87 <s-david.offutt@lwtech.edu>
2025-12-13 20:45:09 -0800

    Add test suite for calculate_strike_angle

--- tests/test_ball.py
+++ tests/test_ball.py
@@ -1,13 +1,27 @@
+import pytest
 import pygame
 
 from bbreaker.Ball import Ball
-from bbreaker.Brick import Brick
 
+# Arrange
+@pytest.mark.parametrize(
+    "start_pos, end_pos, expect_none",
+    [
+        ((100, 200), (100, 100), True),   # Upward drag - invalid
+        ((100, 100), (-20, 120), True),  # Drag angle too shallow to the left - invalid
+        ((100, 100), (220, 120), True),   # Drag angle too shallow to the right - invalid
+        ((100, 100), (100, 120), False), # Straight down - valid
+        ((100, 100), (-18, 121), False), # Downward left - valid
+        ((100, 100), (218, 121), False),  # Downward right - valid
+    ]
+)
 
-def test_handle_one_brick_collision():
-    surface = pygame.display.set_mode((800, 800))
-    ball = Ball(surface, 10, 10)
-    rect = pygame.Rect(100, 100, 60, 60)
-    brick = Brick(rect, number=1)
+def test_calculate_strike_angle(start_pos, end_pos, expect_none):
+    # Act
+    angle = Ball.calculate_strike_angle(start_pos, end_pos)
 
-    assert True
+    # Assert
+    if expect_none:
+        assert angle is None
+    else:
+        assert angle is not None
